d9c791c47a63080984fc57492fc529920f498509
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2021-10-28 14:34:41 -0700

    Create main.js

---
+++ Challenge2/js/main.js
@@ -0,0 +1,7 @@
+function generateMeme() {
+    var image=document.createElement('img');
+    var div=document.getElementById('flex-meme-gen');
+    image.src='https://media1.giphy.com/media/25Ng21kUNfrBMBI3sh/giphy.gif';
+    div.appendChild(image);
+    
+}
